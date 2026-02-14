c14980d8dd0e62af7012272adf85d7312ca34ddd
Gas1212 <ghassengharbi191@gmail.com>
2026-02-14 23:58:39 +0100

    Fix: autoriser les domaines images (Tunisianet, Mytek, Spacenet, API)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'www.tunisianet.com.tn' },
+      { protocol: 'https', hostname: 'www.mytek.tn' },
+      { protocol: 'https', hostname: 'spacenet.tn' },
+      { protocol: 'https', hostname: 'api.toprix.tn' },
+    ],
+  },
 };
 
 export default nextConfig;
